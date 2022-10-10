10819be53a2299570f58b96ffc8e2d8a85a419b4
mario.micallef <mario.micallef@ocado.com>
2022-10-10 19:55:42 +0100

    winner now updates for all cases including draws + tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 pub mod game {
-    use Marker::O;
     use crate::game::CannotMark::{LocationNotEmpty, LocationOutOfBounds};
     use crate::game::Marker::X;
-    use crate::game::Outcome::Winner;
+    use crate::game::Outcome::{Draw, Winner};
+    use Marker::O;
 
     const GRID_SIZE: usize = 3;
 
@@ -23,11 +23,11 @@ pub mod game {
 
         pub fn mark(&mut self, x: usize, y: usize) -> Result<(), CannotMark> {
             if x >= GRID_SIZE || y >= GRID_SIZE {
-                return Err(LocationOutOfBounds)
+                return Err(LocationOutOfBounds);
             }
 
             if self.grid[x][y].is_some() {
-                return Err(LocationNotEmpty)
+                return Err(LocationNotEmpty);
             }
 
             self.mark_grid(x, y);
@@ -41,7 +41,7 @@ pub mod game {
         fn swap_turns(&mut self) {
             self.current_turn = match self.current_turn {
                 X => O,
-                O => X
+                O => X,
             }
         }
 
@@ -50,12 +50,47 @@ pub mod game {
         }
 
         fn update_outcome(&mut self) {
-            let top_left = self.grid[0][0];
-            let top_centre = self.grid[1][0];
-            let top_right = self.grid[2][0];
+            self.check_row(0);
+            self.check_row(1);
+            self.check_row(2);
 
-            if top_left == top_centre && top_centre == top_right {
-                self.outcome = Some(Winner(X));
+            self.check_col(0);
+            self.check_col(1);
+            self.check_col(2);
+
+            let diagonal1 = [self.grid[0][0], self.grid[1][1], self.grid[2][2]];
+            self.update_outcome_on_three_matches(diagonal1);
+
+            let diagonal2 = [self.grid[2][0], self.grid[1][1], self.grid[0][2]];
+            self.update_outcome_on_three_matches(diagonal2);
+
+            self.check_draw();
+        }
+
+        fn check_row(&mut self, i: usize) {
+            self.update_outcome_on_three_matches(self.grid[i])
+        }
+
+        fn check_col(&mut self, i: usize) {
+            self.update_outcome_on_three_matches([
+                self.grid[0][i],
+                self.grid[1][i],
+                self.grid[2][i],
+            ]);
+        }
+
+        fn update_outcome_on_three_matches(&mut self, tiles: [Option<Marker>; 3]) {
+            let first = tiles[0];
+
+            if tiles.iter().all(|tile| tile.is_some() && *tile == first) {
+                self.outcome = Some(Winner(first.unwrap()))
+            }
+        }
+
+        fn check_draw(&mut self) {
+            let all_tiles_used = self.grid.iter().flatten().all(|tile| tile.is_some());
+            if all_tiles_used && self.outcome.is_none() {
+                self.outcome = Some(Draw);
             }
         }
     }
@@ -63,7 +98,7 @@ pub mod game {
     #[derive(Debug, PartialEq)]
     pub enum CannotMark {
         LocationOutOfBounds,
-        LocationNotEmpty
+        LocationNotEmpty,
     }
 
     #[derive(Debug, PartialEq, Clone, Copy)]
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,7 +1,7 @@
-use Marker::{O, X};
-use tic_tac_toe::game::{Game, Marker, Outcome};
 use tic_tac_toe::game::CannotMark::{LocationNotEmpty, LocationOutOfBounds};
-use tic_tac_toe::game::Outcome::Winner;
+use tic_tac_toe::game::Outcome::{Draw, Winner};
+use tic_tac_toe::game::{Game, Marker, Outcome};
+use Marker::{O, X};
 
 #[test]
 fn empty_grid_on_new_game() {
@@ -13,72 +13,6 @@ fn empty_grid_on_new_game() {
         .all(|tile| tile.clone().is_none());
 
     assert!(is_grid_empty);
-}
-
-#[test]
-fn crosses_start() {
-    let game: Game = Game::new();
-    assert_eq!(game.current_turn, X);
-}
-
-#[test]
-fn no_winner_initially() {
-    let game: Game = Game::new();
-    assert_eq!(game.outcome, None);
-}
-
-#[test]
-fn turns_swap_on_each_action() {
-    let mut game: Game = Game::new();
-    game.mark(0,0).unwrap();
-    assert_eq!(game.current_turn, O);
-    game.mark(0,1).unwrap();
-    assert_eq!(game.current_turn, X);
-}
-
-#[test]
-fn mark_updates_grid_0_0() {
-    mark_on_new_game_then_assert_grid_updated(0, 0);
-}
-
-#[test]
-fn mark_updates_grid_0_1() {
-    mark_on_new_game_then_assert_grid_updated(0, 1);
-}
-
-#[test]
-fn mark_updates_grid_0_2() {
-    mark_on_new_game_then_assert_grid_updated(0, 2);
-}
-
-#[test]
-fn mark_updates_grid_1_0() {
-    mark_on_new_game_then_assert_grid_updated(1, 0);
-}
-
-#[test]
-fn mark_updates_grid_1_1() {
-    mark_on_new_game_then_assert_grid_updated(1, 1);
-}
-
-#[test]
-fn mark_updates_grid_1_2() {
-    mark_on_new_game_then_assert_grid_updated(1, 2);
-}
-
-#[test]
-fn mark_updates_grid_2_0() {
-    mark_on_new_game_then_assert_grid_updated(2, 0);
-}
-
-#[test]
-fn mark_updates_grid_2_1() {
-    mark_on_new_game_then_assert_grid_updated(2, 1);
-}
-
-#[test]
-fn mark_updates_grid_2_2() {
-    mark_on_new_game_then_assert_grid_updated(2, 2);
 }
 
 #[test]
@@ -105,13 +39,140 @@ fn cannot_choose_same_location_twice() {
 
 #[test]
 fn x_wins_top_row() {
+    let game = play_and_assert(vec![
+        (0, 0),
+        (0, 1),
+        (1, 0),
+        (0, 2),
+        (2, 0),
+    ]);
+    assert_eq!(game.outcome.unwrap(), Winner(X));
+}
+
+#[test]
+fn o_wins_middle_row() {
+    let game = play_and_assert(vec![
+        (0, 0),
+        (1, 0),
+        (0, 1),
+        (1, 1),
+        (2, 1),
+        (1, 2),
+    ]);
+    assert_eq!(game.outcome.unwrap(), Winner(O))
+}
+
+#[test]
+fn x_wins_bottom_row() {
+    let game = play_and_assert(vec![
+        (2, 0),
+        (0, 1),
+        (2, 1),
+        (0, 2),
+        (2, 2),
+    ]);
+    assert_eq!(game.outcome.unwrap(), Winner(X));
+}
+
+#[test]
+fn o_wins_left_col() {
+    let game = play_and_assert(vec![
+        (1, 1),
+        (0, 0),
+        (1, 2),
+        (1, 0),
+        (2, 2),
+        (2, 0),
+    ]);
+    assert_eq!(game.outcome.unwrap(), Winner(O))
+}
+
+#[test]
+fn x_wins_middle_col() {
+    let game = play_and_assert(vec![
+        (1, 1),
+        (0, 0),
+        (0, 1),
+        (0, 2),
+        (2, 1),
+    ]);
+    assert_eq!(game.outcome.unwrap(), Winner(X));
+}
+
+#[test]
+fn o_wins_right_col() {
+    let game = play_and_assert(vec![
+        (1, 1),
+        (0, 2),
+        (0, 0),
+        (1, 2),
+        (2, 1),
+        (2, 2),
+    ]);
+    assert_eq!(game.outcome.unwrap(), Winner(O))
+}
+
+#[test]
+fn x_wins_first_diagonal() {
+    let game = play_and_assert(vec![
+        (0, 0),
+        (0, 1),
+        (1, 1),
+        (0, 2),
+        (2, 2),
+    ]);
+    assert_eq!(game.outcome.unwrap(), Winner(X));
+}
+
+#[test]
+fn o_wins_second_diagonal() {
+    let game = play_and_assert(vec![
+        (0, 0),
+        (2, 0),
+        (0, 1),
+        (1, 1),
+        (2, 1),
+        (0, 2),
+    ]);
+    assert_eq!(game.outcome.unwrap(), Winner(O))
+}
+
+#[test]
+fn draw() {
+    let game = play_and_assert(vec![
+        (0, 0),
+        (0, 1),
+        (0, 2),
+        (1, 1),
+        (1, 0),
+        (2, 0),
+        (1, 2),
+        (2, 2),
+        (2, 1),
+    ]);
+    assert_eq!(game.outcome.unwrap(), Draw)
+}
+
+fn play_and_assert(positions: Vec<(usize, usize)>) -> Game {
     let mut game: Game = Game::new();
-    game.mark(0, 0).unwrap();
-    game.mark(0, 1).unwrap();
-    game.mark(1, 0).unwrap();
-    game.mark(0, 2).unwrap();
-    game.mark(2, 0).unwrap();
-    assert_eq!(game.outcome.unwrap(), Winner(X))
+    let mut turn_num: u8 = 1;
+    positions.iter().enumerate().for_each(|(i, (x, y))| {
+        assert_eq!(game.outcome, None);
+        assert_eq!(game.current_turn, expected_player(turn_num));
+        game.mark(*x, *y).unwrap();
+        assert_eq!(game.grid[*x][*y], Some(expected_player(turn_num)));
+        turn_num += 1;
+        assert_eq!(game.current_turn, expected_player(turn_num));
+    });
+    game
+}
+
+fn expected_player(turn_num: u8) -> Marker {
+    if turn_num % 2 == 0 {
+        O
+    } else {
+        X
+    }
 }
 
 fn mark_on_new_game_then_assert_grid_updated(x: usize, y: usize) {
