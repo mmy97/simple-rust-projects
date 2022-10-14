a6602375522e8a23229ecd4829175f592fd5bac6
mario.micallef <mario.micallef@ocado.com>
2022-10-14 22:17:42 +0100

    refactor some systems into a plugin and add timer resource to reduce console spam

--- cellular-automata-3d/src/main.rs
+++ cellular-automata-3d/src/main.rs
@@ -3,10 +3,19 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_system(hello_world)
-        .add_startup_system(add_people)
-        .add_system(greet_people)
+        .add_plugin(HelloPlugin)
         .run();
+}
+
+pub struct HelloPlugin;
+struct GreetTimer(Timer);
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people)
+            .add_system(greet_people);
+    }
 }
 
 #[derive(Component)]
@@ -30,9 +39,11 @@ fn add_people(mut commands: Commands) {
         .insert(Name("Zayna Nieves".to_string()));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
